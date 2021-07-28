27ec72da2461460217d27be95a90810624bea9d6
Dennis <dmarlo96@live.ca>
2021-07-28 15:17:22 -0400

    Product Repository

---
+++ TssCodingAssignment.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TssCodingAssignment.DataAccess.Data;
+using TssCodingAssignment.DataAccess.Repository.IRepository;
+using TssCodingAssignment.Models;
+
+namespace TssCodingAssignment.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            var objectFromDb = _db.Products.FirstOrDefault(p => p.Id == product.Id);
+
+            if (objectFromDb != null) 
+            {
+                if (product.ImageUrl != null)
+                {
+                    objectFromDb.ImageUrl = product.ImageUrl;
+                }
+
+                // map the new product to existing product
+                objectFromDb.Name = product.Name;
+                objectFromDb.Description = product.Description;
+                objectFromDb.Price = product.Price;
+                objectFromDb.CategoryId = product.CategoryId;
+
+            }
+        }
+    }
+}
