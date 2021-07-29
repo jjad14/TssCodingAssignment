c25345bb7f5bbcb1a30a3014027755243c5879ae
Dennis <dmarlo96@live.ca>
2021-07-29 16:49:21 -0400

    Add Name and Role to Users Table, and create a new migration

--- TssCodingAssignment.DataAccess/Data/ApplicationDbContext.cs
+++ TssCodingAssignment.DataAccess/Data/ApplicationDbContext.cs
@@ -16,5 +16,6 @@ namespace TssCodingAssignment.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
