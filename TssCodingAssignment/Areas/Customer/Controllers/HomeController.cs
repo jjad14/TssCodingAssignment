38bd09cf9440c18795803b2e080306e35d1bde2e
Dennis <dmarlo96@live.ca>
2021-07-31 18:26:25 -0400

    Adding Items to Cart In Controller (Details Page)

--- TssCodingAssignment/Areas/Customer/Controllers/HomeController.cs
+++ TssCodingAssignment/Areas/Customer/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using TssCodingAssignment.DataAccess.Repository.IRepository;
 using TssCodingAssignment.Models;
@@ -65,10 +67,67 @@ namespace TssCodingAssignment.Areas.Customer.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #region API Calls
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public IActionResult Details(ShoppingCart CartObj)
+        {
+            // So that EF treats this as a new record and it creates it in database
+            CartObj.Id = 0;
+
+            // Add product to cart
+            if (ModelState.IsValid)
+            {
+                // get the id of the currently logged in user
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+                CartObj.ApplicationUserId = claim.Value;
+
+                // retrieve the shopping cart from the database based on the user ID and product Id
+                ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(
+                    u => u.ApplicationUserId == CartObj.ApplicationUserId &&
+                    u.ProductId == CartObj.ProductId,
+                    includeProperties: "Product"
+                 );
+
+                // no record exists in the database for that product, for this user
+                if (cartFromDb == null)
+                {
+                    _unitOfWork.ShoppingCart.Add(CartObj);
+                }
+                else 
+                {
+                    // record exists, update quantity in Db
+                    cartFromDb.Count += CartObj.Count;
+                    _unitOfWork.ShoppingCart.Update(cartFromDb);
+                }
+
+                _unitOfWork.Save();
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                // invalid ModelState, return view
+                var productFromDb = _unitOfWork.Product.GetFirstOrDefault(p => p.Id == CartObj.Id, includeProperties: "Category");
+
+                ShoppingCart cart = new ShoppingCart()
+                {
+                    Product = productFromDb,
+                    ProductId = productFromDb.Id
+                };
+
+                return View(cart);
+            }
+
+        }
+
+        #endregion
     }
-
-
-
 }
 
 
