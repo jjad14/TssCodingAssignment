d48260f78d4678b29734d6e6b1f037d6b82af245
Dennis <dmarlo96@live.ca>
2021-08-01 18:48:58 -0400

    Cart UI, Display cart items, total price, increment/decrement and remove items buttons

--- TssCodingAssignment.Utility/SD.cs
+++ TssCodingAssignment.Utility/SD.cs
@@ -14,5 +14,36 @@ namespace TssCodingAssignment.Utility
 
         public const string ssShoppingCart = "Shopping Cart Session";
 
+
+        // convert all of the html inside the raw data
+        // What this method will do is it will get all of the string and it will convert it into html format
+        // so we can directly display that it is almost the same functionality as @Html.Raw()
+        public static string ConvertToRawHtml(string source)
+        {
+            char[] array = new char[source.Length];
+            int arrayIndex = 0;
+            bool inside = false;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                char let = source[i];
+                if (let == '<')
+                {
+                    inside = true;
+                    continue;
+                }
+                if (let == '>')
+                {
+                    inside = false;
+                    continue;
+                }
+                if (!inside)
+                {
+                    array[arrayIndex] = let;
+                    arrayIndex++;
+                }
+            }
+            return new string(array, 0, arrayIndex);
+        }
     }
 }
---
+++ TssCodingAssignment/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TssCodingAssignment.DataAccess.Repository.IRepository;
+using TssCodingAssignment.Models.ViewModels;
+using TssCodingAssignment.Utility;
+
+namespace TssCodingAssignment.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            // get the id of the currently logged in user
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                OrderHeader = new Models.OrderHeader(),
+                ListCart = _unitOfWork.ShoppingCart
+                    .GetAll(c => c.ApplicationUserId == claim.Value, includeProperties: "Product")
+            };
+
+            // set initial order total to 0
+            ShoppingCartVM.OrderHeader.OrderTotal = 0;
+            ShoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser
+                .GetFirstOrDefault(u => u.Id == claim.Value);
+
+            // calculate price based on quantity
+            foreach (var list in ShoppingCartVM.ListCart)
+            {
+                list.Price = list.Product.Price;
+                ShoppingCartVM.OrderHeader.OrderTotal += list.Product.Price * list.Count;
+
+                // convert description to html format
+                list.Product.Description = SD.ConvertToRawHtml(list.Product.Description);
+
+                // show only 100 characters of the description
+                if (list.Product.Description.Length > 100)
+                {
+                    list.Product.Description = list.Product.Description.Substring(0, 99) + "...";
+                }
+            }
+
+            return View(ShoppingCartVM);
+        }
+
+        public IActionResult Plus(int cartId)
+        {
+            // get cart record
+            var cart = _unitOfWork.ShoppingCart
+                    .GetFirstOrDefault(c => c.Id == cartId, includeProperties: "Product");
+
+            // increment quantity
+            cart.Count += 1;
+
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            // get shopping cart
+            var cart = _unitOfWork.ShoppingCart
+                    .GetFirstOrDefault(c => c.Id == cartId, includeProperties: "Product");
+
+            // if cart quantity is 1, then we have to remove it from the shopping cart
+            if (cart.Count == 1)
+            {
+                // get shopping cart length
+                var cnt = _unitOfWork.ShoppingCart
+                    .GetAll(u => u.ApplicationUserId == cart.ApplicationUserId)
+                    .ToList().Count();
+
+                // remove cart record from shopping cart
+                _unitOfWork.ShoppingCart.Remove(cart);
+                _unitOfWork.Save();
+
+                // update session
+                HttpContext.Session.SetInt32(SD.ssShoppingCart, cnt - 1);
+            }
+            else
+            {
+                // decrement quantity
+                cart.Count -= 1;
+
+                _unitOfWork.Save();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            // get shopping cart
+            var cart = _unitOfWork.ShoppingCart
+                    .GetFirstOrDefault(c => c.Id == cartId, includeProperties: "Product");
+
+            // get shopping cart length
+            var count = _unitOfWork.ShoppingCart
+                .GetAll(u => u.ApplicationUserId == cart.ApplicationUserId)
+                .ToList().Count();
+
+            // remove cart record from shopping cart
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+
+            // update session
+            HttpContext.Session.SetInt32(SD.ssShoppingCart, count - 1);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
